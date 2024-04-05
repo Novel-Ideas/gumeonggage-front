74eebfad8e6f463216c41c46c7d9a29fcb750958
dongyoon7212 <dongyoon7212@naver.com>
2024-04-05 20:03:24 +0900

    Feat: 카테고리 메뉴 페이지 병합, 스타일 조정

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { Reset } from "styled-reset";
 import MenuPage from "./pages/menuPage/MenuPage";
 import HomePage from "./pages/homePage/HomePage";
-import SelectPackageOrStorePage from "./pages/MainPage/SelectPackageOrStorePage/SelectPackageOrStorePage";
-import MenuCategory from "./components/menuCategory/MenuCategory";
+// import SelectPackageOrStorePage from "./pages/MainPage/SelectPackageOrStorePage/SelectPackageOrStorePage";
+// import MenuCategory from "./components/menuCategory/MenuCategory";
 function App() {
     return (
         <>
             <Reset />
             <MenuPage />
             <HomePage />
-            <SelectPackageOrStorePage />
-            <MenuCategory />
+            {/* <SelectPackageOrStorePage /> */}
+            {/* <MenuCategory /> */}
         </>
     );
 }
--- src/components/menuCategory/style.js
+++ src/components/menuCategory/style.js
@@ -1,44 +1,37 @@
 import { css } from "@emotion/react";
 
 export const layout = css`
-    background-color: #dbdbdb;
-    width: 50%;
-    height: 250px;
-`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+`;
 
 export const header = css`
     box-sizing: border-box;
     display: flex;
-    margin-left: 30px;
     align-items: center;
-`
+    margin-top: 20px;
+`;
 
-export const font = css`
-    margin-top: 10px;
+export const menuFont = css`
+    font-size: 45px;
+    font-weight: 700;
     color: #222;
     cursor: default;
-`
+`;
 
-export const font2 = css`
-    margin-top: 10px;
-    margin-left: 10px;
+export const categoryFont = css`
+    margin-left: 5px;
+    font-size: 45px;
+    font-weight: 300;
     color: #222;
-    font-weight: 400;
     cursor: default;
-`
+`;
 
-export const categorylayout = css`
+export const categoryLayout = css`
     box-sizing: border-box;
-    padding-left: 30px;
     display: flex;
     align-items: center;
-    justify-content: space-around;
 `;
-
-export const category = css`
-    padding-right: 20px;
-`;
-
-
-
-
