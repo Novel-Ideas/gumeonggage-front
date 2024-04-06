e1275218b33fdafa7bbaee70b13022b9c5de5256
dongyoon7212 <dongyoon7212@naver.com>
2024-04-06 22:49:13 +0900

    Feat: SelectPage -> MenuPage 구현

--- src/pages/mainPage/selectPackageOrStorePage/SelectPackageOrStorePage.jsx
+++ src/pages/mainPage/selectPackageOrStorePage/SelectPackageOrStorePage.jsx
@@ -4,16 +4,21 @@ import { MdOutlineTakeoutDining } from "react-icons/md";
 import { MdOutlineTableRestaurant } from "react-icons/md";
 import { AiOutlineHome } from "react-icons/ai";
 import PageLayout from "../../../components/pageComponents/PageLayout";
+import { useNavigate } from "react-router-dom";
 
 function SelectPackageOrStorePage() {
+    const navigate = useNavigate();
+    const handleSelectButtonClick = () => {
+        navigate("/menu");
+    };
     return (
         <PageLayout>
             <div css={s.layout}>
-                <button css={s.selectWay}>
+                <button css={s.selectWay} onClick={handleSelectButtonClick}>
                     <MdOutlineTakeoutDining />
                     <h1>포장</h1>
                 </button>
-                <button css={s.selectWay}>
+                <button css={s.selectWay} onClick={handleSelectButtonClick}>
                     <MdOutlineTableRestaurant />
                     <h1>매장</h1>
                 </button>
