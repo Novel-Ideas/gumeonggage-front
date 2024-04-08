692f6a30e09fe596a257fe7d0d2b080116c4303d
dongyoon7212 <dongyoon7212@naver.com>
2024-04-08 15:07:25 +0900

    Feat: menuPage 주문 리스트 레이아웃 구현

--- src/pages/menuPage/MenuPage.jsx
+++ src/pages/menuPage/MenuPage.jsx
@@ -2,6 +2,7 @@
 import * as s from "./style";
 import PageLayout from "../../components/pageComponents/PageLayout";
 import MenuCategoryPage from "../../components/menuComponents/menuCategory/MenuCategory";
+import OrderListComponent from "../../components/menuComponents/orderListComponent/OrderListComponent";
 
 function MenuPage(props) {
     return (
@@ -19,7 +20,9 @@ function MenuPage(props) {
                     </div>
                 </div>
                 <div css={s.orderLayout}>
-                    <div css={s.orderMenuLayout}>주문 메뉴</div>
+                    <div css={s.orderMenuLayout}>
+                        <OrderListComponent />
+                    </div>
                     <div css={s.orderButtonLayout}>
                         <button css={s.orderButton}>
                             주문하기<p>12,900원</p>
