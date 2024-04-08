4316fa31eb137dc92f30d9d6da852fb83779b3cd
jayhyeon12 <jhkk0124@icloud.com>
2024-04-08 11:48:34 +0900

    Feat: 놀거리, 지도, 놀거리 추천 컴포넌트 구성

--- src/components/playComponent/playMap/style.js
+++ src/components/playComponent/playMap/style.js
@@ -2,15 +2,10 @@ import { css } from "@emotion/react";
 
 export const layout = css`
     box-sizing: border-box;
-    width: 750px;
-    height: 350px;
-`;
-
-export const button = css`
     display: flex;
     justify-content: center;
     align-items: center;
-    border-radius: 30px;
-    width: 150px;
-    height: 50px;
+    border-radius: 40px;
+    width: 400px;
+    height: 250px;
 `;
