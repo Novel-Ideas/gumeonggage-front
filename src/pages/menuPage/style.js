b703fa9442ca856782bdf3103bcc774f428bf834
dongyoon7212 <dongyoon7212@naver.com>
2024-04-08 09:43:35 +0900

    Fix: menuPage layout 수정

--- src/pages/menuPage/style.js
+++ src/pages/menuPage/style.js
@@ -13,13 +13,13 @@ export const container = css`
 
 export const categoryLayout = css`
     position: relative;
-    height: 25%;
+    height: 30%;
     display: flex;
     padding-left: 40px;
 `;
 
 export const menuLayout = css`
-    height: 75%;
+    height: 70%;
     background-color: coral;
 `;
 
