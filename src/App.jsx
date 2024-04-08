7b8b95855bd56fbafe9fd041cfe77882791dfa73
seohyeonwook <skut01541@gmail.com>
2024-04-08 11:34:54 +0900

    카드결제 및 포인트 적립 화면구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Reset } from "styled-reset";
 import MenuRoute from "./routes/MenuRoute";
 import PaymentMethod from "./pages/payment Method/PaymentMethod";
+import PayList from "./pages/payList/PayList";
+import PointAccumulation from "./pages/pointAccumulation/PointAccumulation";
 
 function App() {
     return (
         <>
             {/* <Reset />
             <MenuRoute /> */}
-            <PaymentMethod />
+            {/* <PaymentMethod /> */}
+            {/* <PayList /> */}
+            <PointAccumulation />
         </>
     );
 }
