70fc0eec36e5e05e71c394ffdf4e6d7ae323c416
dongyoon7212 <dongyoon7212@naver.com>
2024-04-09 12:09:12 +0900

    Feat: AdminAuthPage 스타일 수정

--- src/pages/adminPages/adminAuthPage/style.js
+++ src/pages/adminPages/adminAuthPage/style.js
@@ -1,38 +1,66 @@
 import { css } from "@emotion/react";
 
 export const layout = css`
-    box-sizing: border-box;
-    border: 1px solid #dbdbdb;
+    width: 100%;
+    height: 100%;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-    width: 80vw;
-    height: 80vh;
+`;
 
+export const signinBox = css`
+    box-sizing: border-box;
+    border: 2px solid rgb(252, 10, 86);
+    border-radius: 30px;
+    background-color: rgb(252, 10, 86);
+    padding: 50px;
+    width: 40%;
+    height: 40%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 5px 5px 5px #898989ff;
+
+    & > h1 {
+        font-size: 60px;
+        color: white;
+        font-weight: 700;
+    }
 `;
 
 export const inputInfo = css`
     display: flex;
     justify-content: center;
     align-items: center;
-    padding: 20px;
+    padding: 10px;
 
-    & > input{
-        width: 20vw;
-        height: 4vh;
-        
+    & > input {
+        width: 250px;
+        height: 40px;
+        border: none;
+        border-radius: 10px;
+        outline: none;
+        font-size: 25px;
+        padding: 5px 10px;
     }
-`
+`;
 
 export const loginButton = css`
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    margin-top: 30px;
+    color: rgb(252, 10, 86);
+    font-weight: 700;
+    font-size: 30px;
+    box-sizing: border-box;
+    border: 2px solid rgb(252, 10, 86);
+    border-radius: 20px;
+    background-color: white;
+    padding: 20px 190px;
+    box-shadow: 5px 5px 5px #898989ff;
 
-    & > button {
-        font-size: 16px;
-        width: 150px;
-        height: 40px;
+    &:active {
+        background-color: rgb(252, 10, 86);
+        color: white;
     }
-
-`
+`;
