a1451a792c6aa6c3f370ff40d2e5df36d2149afb
jayhyeon12 <jhkk0124@icloud.com>
2024-04-09 11:46:38 +0900

    Feat: 포인트 체크 페이지 구현

---
+++ src/pages/pointPage/pointCheck/style.js
@@ -0,0 +1,33 @@
+import { css } from "@emotion/react";
+
+export const layout = css`
+    box-sizing: border-box;
+    border: 1px solid black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 700px;
+    height: 400px;
+`;
+
+export const signupAndAccumul= css`
+    box-sizing: border-box;
+    border: 1px solid black;
+    align-items: center;
+    justify-content: space-around;
+    width: 250px;
+    height: 140px;
+    font-size: 25px;
+`;
+
+export const rejectButton = css`
+    box-sizing:border-box;
+    border: 1px solid black;
+    font-size: 25px;
+    width: 250px;
+    height: 140px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+`;
