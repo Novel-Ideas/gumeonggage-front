064be087016f16135784e00b015acd9ca9bfdcfe
ez0603 <dlwldjs3132@naver.com>
2024-04-10 01:08:40 +0900

    Feat: AdminMainPageTop3 component 생성

---
+++ src/components/adminMainPageTop3/AdminMainPageTop3.jsx
@@ -0,0 +1,30 @@
+/** @jsxImportSource @emotion/react */ // aside css
+import * as s from "./style";
+import { FaCrown } from "react-icons/fa6";
+
+function AdminMainPageTop3(props) {
+    return (
+        <div css={s.layout}>
+            <div css={s.container}>
+                <h1 css={s.font}><FaCrown css={s.icon}/> 1위</h1>
+                <div css={s.top3}>img</div>
+                <h1 css={s.menu}>치즈버거</h1>
+            </div>
+            <div css={s.container}>
+                <h1 css={s.font}>2위</h1>
+                <div css={s.top3}>img</div>
+                <h1 css={s.menu}>치즈버거</h1>
+            </div>
+            <div css={s.container}>
+                <h1 css={s.font}>3위</h1>
+                <div css={s.top3}>img</div>
+                <h1 css={s.menu}>치즈버거</h1>
+            </div>
+           
+                {/* <div css={s.top3}>d</div>
+                <div css={s.top3}>d</div> */}
+        </div>
+    );
+}
+
+export default AdminMainPageTop3;
---
+++ src/components/adminMainPageTop3/style.js
@@ -0,0 +1,45 @@
+import { css } from "@emotion/react";
+
+export const layout = css`
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+`;
+
+export const container = css`
+    width: 25%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const icon = css`
+    color: #ffdd19;
+`
+
+
+export const font = css`
+    padding-bottom: 10px;
+    font-size: 30px;
+    font-weight: 600;
+    color: #555;
+`;
+
+export const top3 = css`
+    border: 1px solid black;
+    width: 100%;
+    height: 180px;
+    background-color: coral;
+    border-radius: 10px;
+`;
+
+export const menu = css`
+    padding: 15px;
+    font-size: 25px;
+    
+`;
