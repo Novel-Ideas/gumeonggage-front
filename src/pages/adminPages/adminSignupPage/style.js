50e56f9d7e1f2f2f6c6b9a65ea6351a404dff13b
jayhyeon12 <jhkk0124@icloud.com>
2024-04-09 10:32:27 +0900

    Feat: 관리자 회원가입 화면 구성

---
+++ src/pages/adminPages/adminSignupPage/style.js
@@ -0,0 +1,45 @@
+import { css } from "@emotion/react";
+
+export const layout = css`
+    box-sizing: border-box;
+    border: 1px solid black;
+    width: 50vw;
+    height: 50vh;
+    align-items: center;
+    justify-content: center;
+    
+    & > h1 {
+        display: flex;
+        justify-content: center;
+        font-size: 35px;
+        
+    }
+    
+`;
+
+export const inputInfo = css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 5px;
+
+    & > input {
+        width: 150px;
+        height: 30px;
+    }
+`;
+
+export const signupButton = css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+    
+    & > * {
+        font-size: 20px;
+        width: 160px;
+        height: 40px;
+    }
+
+`;
+
--- src/routes/HomeRoute.jsx
+++ src/routes/HomeRoute.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import MenuRoute from "./MenuRoute";
 import AdminRoute from "./AdminRoute";
 import AdminAuthPage from "../pages/adminPages/adminAuthPage/AdminAuthPage";
+import AdminSignupPage from "../pages/adminPages/adminSignupPage/AdminSignupPage";
 
 function HomeRoute(props) {
     return (
@@ -12,6 +13,7 @@ function HomeRoute(props) {
                 <Route path="/menu/*" element={
                     <MenuRoute />
                 } />
+                <Route path="/adminsignup" element={<AdminSignupPage /> } />
                 <Route path="/admin/*" element={
                     <AdminRoute />
                 } />
