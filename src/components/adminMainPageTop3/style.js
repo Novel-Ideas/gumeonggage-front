d34287f8b876a1f4b659c185983e3344287a4c88
ez0603 <dlwldjs3132@naver.com>
2024-04-11 14:47:18 +0900

    Feat: 임시 커밋

--- src/components/adminMainPageTop3/style.js
+++ src/components/adminMainPageTop3/style.js
@@ -14,7 +14,7 @@ export const container = css`
     height: 100%;
     display: flex;
     flex-direction: column;
-    align-items: center;
+    align-items: center                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    ;
     justify-content: center;
 `;
 
