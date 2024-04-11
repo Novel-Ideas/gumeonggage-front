0490b83bc8857685ddf1657dfd755649f737c57d
seohyeonwook <skut01541@gmail.com>
2024-04-11 10:31:23 +0900

    components- AuthPageInput / Pages- PasswordEditPage / hooks- useEditPassword 생성

---
+++ src/pages/passwordEditPage/PasswordEditPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PasswordEditPage() {
+    return (
+        <div>
+            <h1>비밀번호 변경</h1>
+            <ㅁ
+            
+        </div>
+    );
+}
+
+export default PasswordEditPage;
