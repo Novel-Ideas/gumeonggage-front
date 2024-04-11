488e9e3d169f2d5e3d3e0e7027d20fbaa87f9b78
seohyeonwook <skut01541@gmail.com>
2024-04-11 14:54:30 +0900

    Feat : useAuthCheck 생성

--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import AdminMenuAdd from "../pages/adminPages/adminMenuAdd/AdminMenuAdd"
 import AdminMainPage from "../pages/adminPages/adminMainPage/AdminMainPage";
+import PasswordEditPage from "../pages/passwordEditPage/PasswordEditPage";
 
 function AdminRoute(props) {
     return (
@@ -12,6 +13,7 @@ function AdminRoute(props) {
             <Route path="/update" />
             <Route path="/delete" />
             <Route path="/feedback" />
+            <Route path="/passwordedit" element={<PasswordEditPage />} />  
         </Routes>
     );
 }
