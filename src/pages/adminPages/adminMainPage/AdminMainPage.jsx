0cd52279ed52642550f51c923c9627c8276bab9c
dongyoon7212 <dongyoon7212@naver.com>
2024-04-12 09:51:29 +0900

    Feat: 관리자 모드 매출 그래프 데이터 연동 완료

--- src/pages/adminPages/adminMainPage/AdminMainPage.jsx
+++ src/pages/adminPages/adminMainPage/AdminMainPage.jsx
@@ -4,7 +4,7 @@ import AdminPageLayout from "../../../components/pageComponents/adminPageLayout/
 import AdminMainPageTop3 from "../../../components/adminMainPageTop3/AdminMainPageTop3";
 import { useAuthCheck } from "../../../hooks/useAuthCheck";
 import { useEffect, useState } from "react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { getTop3Request } from "../../../apis/api/menuList";
 import {
     Bar,
@@ -17,9 +17,22 @@ import {
     XAxis,
     YAxis,
 } from "recharts";
+import { getSalesRequest } from "../../../apis/api/salesApi";
 
 function AdminMainPage() {
     const [lanking, setLanking] = useState([]);
+    const [sales, setSales] = useState([]);
+    const salesQuery = useQuery(["salesQuery"], getSalesRequest, {
+        retry: 0,
+        refetchOnWindowFocus: false,
+        onSuccess: (response) => {
+            console.log("salesQuery", response.data);
+            setSales(() => response.data);
+        },
+        onError: (error) => {
+            console.log("salesQuer", error);
+        },
+    });
 
     const getLankingMutation = useMutation({
         mutationKey: "getLankingMutation",
@@ -32,81 +45,6 @@ function AdminMainPage() {
             }
         },
     });
-
-    const data = [
-        {
-            name: "1월",
-            uv: 2400,
-            pv: 2400,
-            amt: 2400,
-        },
-        {
-            name: "2월",
-            uv: 3000,
-            pv: 3000,
-            amt: 3000,
-        },
-        {
-            name: "3월",
-            uv: 2000,
-            pv: 2000,
-            amt: 2000,
-        },
-        {
-            name: "4월",
-            uv: 2780,
-            pv: 2780,
-            amt: 2780,
-        },
-        {
-            name: "5월",
-            uv: 1890,
-            pv: 1890,
-            amt: 1890,
-        },
-        {
-            name: "6월",
-            uv: 2390,
-            pv: 2390,
-            amt: 2390,
-        },
-        {
-            name: "7월",
-            uv: 3490,
-            pv: 3490,
-            amt: 3490,
-        },
-        {
-            name: "8월",
-            uv: 1490,
-            pv: 1490,
-            amt: 1490,
-        },
-        {
-            name: "9월",
-            uv: 2425,
-            pv: 2425,
-            amt: 2425,
-        },
-        {
-            name: "10월",
-            uv: 3490,
-            pv: 3490,
-            amt: 3490,
-        },
-        {
-            name: "11월",
-            uv: 3490,
-            pv: 3490,
-            amt: 3490,
-        },
-        {
-            name: "12월",
-            uv: 3490,
-            pv: 3490,
-            amt: 3490,
-        },
-    ];
 
     useEffect(() => {
         getLankingMutation.mutate();
@@ -127,22 +65,24 @@ function AdminMainPage() {
                                 <ComposedChart
                                     // width={730}
                                     // height={250}
-                                    data={data}
+                                    data={sales}
                                 >
-                                    <XAxis dataKey="name" />
-                                    <YAxis />
+                                    <XAxis dataKey="month" />
+                                    <YAxis name="aocnf" />
                                     <Tooltip />
                                     <Legend />
                                     <CartesianGrid stroke="#f5f5f5" />
                                     <Bar
-                                        dataKey="pv"
+                                        dataKey="totalSales"
                                         barSize={20}
+                                        name="매출"
                                         fill="#8abdf3"
                                     />
                                     <Line
                                         type="monotone"
-                                        dataKey="uv"
+                                        dataKey="totalSales"
                                         stroke="#ff7300"
+                                        legendType="none"
                                     />
                                 </ComposedChart>
                             </ResponsiveContainer>
