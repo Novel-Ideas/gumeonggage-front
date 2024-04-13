778271e3e9acb793b8d0995c8cc0b6db33821022
dongyoon7212 <dongyoon7212@naver.com>
2024-04-13 16:49:04 +0900

    Feat: 큰글씨 모드 레이아웃 카테고리 구현

--- src/components/bigComponents/bigCategoryButton/BigCategoryButton.jsx
+++ src/components/bigComponents/bigCategoryButton/BigCategoryButton.jsx
@@ -1,14 +1,11 @@
 /**@jsxImportSource @emotion/react */
 import * as s from "./style";
 
-function BigCategoryButton({ icon, categoryName }) {
-    
+function BigCategoryButton({ icon, categoryName, onClick }) {
     return (
         <div css={s.categoryLayout}>
-            <button css={s.category}>
-                <div css={s.imglayout}>
-                    {icon}
-                </div>
+            <button css={s.category} onClick={onClick}>
+                <div css={s.imglayout}>{icon}</div>
                 <div css={s.categoryTitleLayout}>
                     <h1 css={s.categoryfont}>{categoryName}</h1>
                 </div>
