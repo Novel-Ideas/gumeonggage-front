e8594b4432207457ffd9ad312b99e946efbc5163
seohyeonwook <skut01541@gmail.com>
2024-04-17 15:29:59 +0900

    회원가입 페이지 css수정

--- src/pages/adminPages/adminSignupPage/AdminSignupPage.jsx
+++ src/pages/adminPages/adminSignupPage/AdminSignupPage.jsx
@@ -5,6 +5,7 @@ import * as s from "./style";
 import { signupRequest } from "../../../apis/api/signup";
 import { useNavigate } from "react-router-dom";
 import AuthPageInput from "../../../components/authPageInput/AuthPageInput";
+import PageLayout from "../../../components/pageComponents/pageLayout/PageLayout";
 
 function AdminSignupPage() {
     const navigate = useNavigate();
@@ -60,19 +61,23 @@ function AdminSignupPage() {
 
     }
     return (
-        <div css={s.layout}>
-            <h1>회원가입</h1>
-            <div css={s.inputInfo}>
-                <AuthPageInput type={"text"} name={"username"} placeholder={"사용자이름"} value={username} onChange={userNameChange} message={usernameMessage} />
-                <AuthPageInput type={"password"} name={"password"} placeholder={"비밀번호"} value={password} onChange={passswordChange} message={passwordMessage} />
-                <AuthPageInput type={"password"} name={"checkPassword"} placeholder={"비밀번호 확인"} value={checkPassword} onChange={checkPasswordChange} message={checkPasswordMessage} />
-                <AuthPageInput type={"text"} name={"name"} placeholder={"성명"} value={name} onChange={nameChange} message={nameMessage} />
-                <AuthPageInput type={"text"} name={"email"} placeholder={"이메일"} value={email} onChange={emailChange} message={emailMessage} />
-            </div>
-            <div css={s.signupButton}>
-                <button onClick={handleSignupSubmit}>가입하기</button>
-            </div>
-        </div>  
+        <PageLayout>
+            <div css={s.layout}>
+                    <div css={s.signupBox}>
+                        <h1>회원가입</h1>
+                        <div css={s.inputInfo}>
+                            <AuthPageInput type={"text"} name={"username"} placeholder={"사용자이름"} value={username} onChange={userNameChange} message={usernameMessage} />
+                            <AuthPageInput type={"password"} name={"password"} placeholder={"비밀번호"} value={password} onChange={passswordChange} message={passwordMessage} />
+                            <AuthPageInput type={"password"} name={"checkPassword"} placeholder={"비밀번호 확인"} value={checkPassword} onChange={checkPasswordChange} message={checkPasswordMessage} />
+                            <AuthPageInput type={"text"} name={"name"} placeholder={"성명"} value={name} onChange={nameChange} message={nameMessage} />
+                            <AuthPageInput type={"text"} name={"email"} placeholder={"이메일"} value={email} onChange={emailChange} message={emailMessage} />
+                        </div>
+                    </div>
+                <div>
+                    <button css={s.signupButton} onClick={handleSignupSubmit}>가입하기</button>
+                </div>
+            </div>  
+        </PageLayout>
     );
 }
 
