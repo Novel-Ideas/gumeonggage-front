0b6ebe5785c61be9e0656b3aade317b162963276
ez0603 <dlwldjs3132@naver.com>
2024-04-17 17:50:20 +0900

    Feat: 후기 작성 선택 페이지 구현중

--- src/pages/feedbackPage/feedbackWritePage/FeedbackWritePage.jsx
+++ src/pages/feedbackPage/feedbackWritePage/FeedbackWritePage.jsx
@@ -1,13 +1,38 @@
 /** @jsxImportSource @emotion/react */
+import { useState } from "react";
 import PageLayout from "../../../components/pageComponents/pageLayout/PageLayout";
 import * as s from "./style";
 import { useNavigate } from "react-router-dom";
+import { useMutation, useQuery } from "react-query";
+import { addFeedbackRequest } from "../../../apis/api/feedback";
+import useFeedback from "../../../hooks/useFeedback";
 
 function FeedbackWritePage() {
+    const [ feedbackList, setFeedbackList ] = useState([]);
     const navigate = useNavigate();
-    const handleSubmitClick = () => {
-        navigate("/menu/feedback/ok");
-    };
+
+    const feedbackMutation = useMutation({
+        mutationKey: "feedbackMutation",
+        mutationFn: addFeedbackRequest,
+        retry:0,
+        onSuccess: (response) => {
+            alert("등록완료")
+            navigate("/menu/feedback/ok");
+        },
+        onError: (error) => {
+            console.log(error)
+        }
+    })
+
+    const handleFeedbackSubmitClick = () => {
+        feedbackMutation.mutate()
+    }
+
+    const handleChange = (e) => {
+        setFeedbackList(e.target.value)
+        console.log(e.target.value)
+    }
+
     return (
         <PageLayout>
             <div css={s.layout}>
@@ -21,18 +46,23 @@ function FeedbackWritePage() {
                                     type="radio"
                                     name="salinity"
                                     id="watery"
+                                    onChange={() => handleChange()}
                                 />
                                 <label for="watery">맛없음</label>
                                 <input
                                     type="radio"
                                     name="salinity"
                                     id="proper1"
+                                    // value={answer1.value}
+                                    onChange={handleChange}
                                 />
                                 <label for="proper1">보통</label>
                                 <input
                                     type="radio"
                                     name="salinity"
                                     id="salty"
+                                    // value={answer1.value}
+                                    onChange={handleChange}
                                 />
                                 <label for="salty">맛있음</label>
                             </div>
@@ -40,32 +70,49 @@ function FeedbackWritePage() {
                         <div css={s.feedbackContents}>
                             <h1>음식의 양이 적당했나요?</h1>
                             <div css={s.radioButton}>
-                                <input type="radio" name="amount" id="little" />
+                                <input type="radio" name="amount" id="little" 
+                                //   value={answer2.value}
+                                  onChange={handleChange}
+                                />
                                 <label for="little">적음</label>
                                 <input
                                     type="radio"
                                     name="amount"
                                     id="proper2"
+                                    // value={answer2.value}
+                                    onChange={handleChange}
                                 />
                                 <label for="proper2">적당함</label>
-                                <input type="radio" name="amount" id="many" />
+                                <input type="radio" name="amount" id="many" 
+                                //    value={answer2.value}
+                                   onChange={handleChange}
+                                />
                                 <label for="many">많음</label>
                             </div>
                         </div>
                         <div css={s.feedbackContents}>
                             <h1>직원들이 친절했나요?</h1>
                             <div css={s.radioButton}>
-                                <input type="radio" name="kind" id="unkind" />
+                                <input type="radio" name="kind" id="unkind" 
+                                //    value={answer3.value}
+                                   onChange={handleChange}
+                                />
                                 <label for="unkind">불친절함</label>
-                                <input type="radio" name="kind" id="proper3" />
+                                <input type="radio" name="kind" id="proper3" 
+                                //    value={answer3.valu}
+                                   onChange={handleChange}
+                                />
                                 <label for="proper3">보통</label>
-                                <input type="radio" name="kind" id="kind" />
+                                <input type="radio" name="kind" id="kind" 
+                                //    value={answer3.valu}
+                                   onChange={handleChange}
+                                />
                                 <label for="kind">친절함</label>
                             </div>
                         </div>
                     </div>
                 </div>
-                <button css={s.checkButton} onClick={handleSubmitClick}>
+                <button css={s.checkButton} onClick={handleFeedbackSubmitClick}>
                     확인
                 </button>
             </div>
