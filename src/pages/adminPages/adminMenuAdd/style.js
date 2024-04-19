532cf470a6aebf3109888a59541bf3cb18d13ffc
ez0603 <dlwldjs3132@naver.com>
2024-04-19 11:10:38 +0900

    Feat: 관리자 모드 메뉴 수정 완료

--- src/pages/adminPages/adminMenuAdd/style.js
+++ src/pages/adminPages/adminMenuAdd/style.js
@@ -19,7 +19,7 @@ export const title = css`
     border-bottom: 2px solid #222;
     color: #222;
     width: 90%;
-    height: 30%;
+    height: 40%;
     font-size: 40px;
     font-weight: 700;
 `;
