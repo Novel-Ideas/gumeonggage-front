43a8e71bde561228d340620c15fa78671517f50d
ez0603 <dlwldjs3132@naver.com>
2024-04-19 11:24:27 +0900

    Feat: 관리자 회원 관리 수정 완료

--- src/components/adminUserSearch/style.js
+++ src/components/adminUserSearch/style.js
@@ -19,7 +19,7 @@ export const title = css`
     border-bottom: 2px solid #222;
     color: #222;
     width: 90%;
-    height: 30%;
+    height: 40%;
     font-size: 40px;
     font-weight: 700;
 `;
@@ -86,9 +86,12 @@ export const askTitle = css`
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 100%;
+    width: 90;
     height: 8%;
+    margin-top: 0;
+    margin-bottom: 0;
     color: white;
+    padding-left: 0;
 
     & > li {
         text-align: center;
@@ -137,7 +140,7 @@ export const askList = css`
     width: 100%;
     height: 8%;
     color: #333;
-    
+    padding-left: 0;
      & > li {
         text-align: center;
     }
