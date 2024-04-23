624b445db961707a08c86a5ae70bb0f51c9d0059
dongyoon7212 <dongyoon7212@naver.com>
2024-04-23 17:49:10 +0900

    Feat: 리스트 별 구글맵 연동

--- src/components/playComponent/playList/PlayList.jsx
+++ src/components/playComponent/playList/PlayList.jsx
@@ -2,23 +2,24 @@
 import { useRecoilState } from "recoil";
 import * as s from "./style";
 import { playDataListState } from "../../../atoms/playDataListAtom";
-import { useSearchParams } from "react-router-dom";
+import { playDataState } from "../../../atoms/playDataAtom";
+import { useNavigate } from "react-router-dom";
 
 function PlayList() {
     const [playListData, setPlayListData] = useRecoilState(playDataListState);
+    const [playData, setPlayData] = useRecoilState(playDataState);
+    const navigate = useNavigate();
 
     const handleSubmitClick = (id) => {
-        console.log(id);
-
-        const playList = playListData.filter((data) => data.id === id)[0]
-        console.log(playList)
+        const playList = playListData.filter((data) => data.id === id)[0];
+        setPlayData(() => playList);
+        navigate("/menu/playlist/map");
     };
-
     return (
         <div css={s.layout}>
             <div css={s.listContainer}>
                 <ul css={s.listLayout}>
-                    {playListData.map((data) => (
+                    {playListData.map(  (data) => (
                         <li
                             css={s.list}
                             key={data.id}
