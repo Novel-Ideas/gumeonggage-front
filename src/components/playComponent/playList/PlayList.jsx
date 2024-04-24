a2d45bade4523893359c8249f4be8082ef04d11a
ez0603 <dlwldjs3132@naver.com>
2024-04-24 09:34:16 +0900

    Feat: 관광지 페이지 리스트 구현중

--- src/components/playComponent/playList/PlayList.jsx
+++ src/components/playComponent/playList/PlayList.jsx
@@ -9,20 +9,20 @@ import { FaStar } from "react-icons/fa";
 import { IoIosPhonePortrait } from "react-icons/io";
 import { FaRegClock } from "react-icons/fa6";
 import { MdPlace } from "react-icons/md";
-import PlayMap from "../playMap/PlayMap"
+import PlayMap from "../playMap/PlayMap";
 
 function PlayList() {
     const [playListData, setPlayListData] = useRecoilState(playDataListState);
-    const [selectedPlayList, setSelectedPlayList] = useState('');
+    const [selectedPlayList, setSelectedPlayList] = useState("");
     const handlePageClick = () => {
-        <PlayMap />
+        <PlayMap />;
     };
 
     const handleSubmitClick = (id) => {
         console.log(id);
-        
-        const playList = playListData.filter((data) => data.id === id)[0]
-        console.log(playListData)
+
+        const playList = playListData.filter((data) => data.id === id)[0];
+        console.log(playListData);
         if (playListData.id !== playList?.id) {
             setSelectedPlayList(playList);
         }
@@ -30,9 +30,7 @@ function PlayList() {
     };
     console.log(playListData);
 
-    useEffect(() => {
-
-    }, [playListData])
+    useEffect(() => {}, [playListData]);
 
     return (
         <div css={s.layout}>
@@ -48,27 +46,94 @@ function PlayList() {
                         </li>
                     ))}
                 </ul>
-        </div>
-                <div css={s.contentLayout}>
-                    <div css={s.container}>
-                        <div css={s.top}>
-                            <button css={s.mapButton} onClick={() => handlePageClick()}>지도 보기</button>
-                        </div>
-                    <div css={s.bodyContainer}>
-                        {selectedPlayList &&(
-                        <> 
-                        <div css={s.name} onClick={() => handleSubmitClick(selectedPlayList.displayName)}>{selectedPlayList.displayName.text !== undefined ? selectedPlayList.displayName.text: "정보없음"}</div> 
-                        <div css={s.text}><FaStar /> 별점</div>
-                        <div css={s.text} onClick={() => handleSubmitClick(selectedPlayList.formattedAddress)}><MdPlace /> {selectedPlayList.formattedAddress !== undefined ? selectedPlayList.formattedAddress : "정보없음" }</div>
-                        <div css={s.text} onClick={() => handleSubmitClick(selectedPlayList.nationalPhoneNumber)}><IoIosPhonePortrait /> {selectedPlayList.nationalPhoneNumber !== undefined ? selectedPlayList.nationalPhoneNumber : "정보없음"}</div>
-                        <div css={s.text} onClick={() => handleSubmitClick(selectedPlayList.periods)}><FaRegClock /> {selectedPlayList.periods !== undefined ? selectedPlayList.periods : "정보없음"}</div>
-                        <div css={s.review}onClick={() => handleSubmitClick(selectedPlayList.text)}>Review {selectedPlayList.reviews.text !== undefined ? selectedPlayList.reviews.text : "정보없음"}</div>
-                        </>
-                        )}
+            </div>
+            <div css={s.contentLayout}>
+                <div css={s.container}>
+                    <div css={s.top}>
+                        <button
+                            css={s.mapButton}
+                            onClick={() => handlePageClick()}
+                        >
+                            지도 보기
+                        </button>
                     </div>
+                    <div css={s.bodyContainer}>
+                        {selectedPlayList && (
+                            <>
+                                <div
+                                    css={s.name}
+                                    onClick={() =>
+                                        handleSubmitClick(
+                                            selectedPlayList.displayName
+                                        )
+                                    }
+                                >
+                                    {selectedPlayList.displayName.text !==
+                                    undefined
+                                        ? selectedPlayList.displayName.text
+                                        : "정보없음"}
+                                </div>
+                                <div css={s.text}>
+                                    <FaStar /> 별점
+                                </div>
+                                <div
+                                    css={s.text}
+                                    onClick={() =>
+                                        handleSubmitClick(
+                                            selectedPlayList.formattedAddress
+                                        )
+                                    }
+                                >
+                                    <MdPlace />{" "}
+                                    {selectedPlayList.formattedAddress !==
+                                    undefined
+                                        ? selectedPlayList.formattedAddress
+                                        : "정보없음"}
+                                </div>
+                                <div
+                                    css={s.text}
+                                    onClick={() =>
+                                        handleSubmitClick(
+                                            selectedPlayList.nationalPhoneNumber
+                                        )
+                                    }
+                                >
+                                    <IoIosPhonePortrait />{" "}
+                                    {selectedPlayList.nationalPhoneNumber !==
+                                    undefined
+                                        ? selectedPlayList.nationalPhoneNumber
+                                        : "정보없음"}
+                                </div>
+                                <div
+                                    css={s.text}
+                                    onClick={() =>
+                                        handleSubmitClick(
+                                            selectedPlayList.periods
+                                        )
+                                    }
+                                >
+                                    <FaRegClock />{" "}
+                                    {selectedPlayList.periods !== undefined
+                                        ? selectedPlayList.periods
+                                        : "정보없음"}
+                                </div>
+                                <div
+                                    css={s.review}
+                                    onClick={() =>
+                                        handleSubmitClick(selectedPlayList.text)
+                                    }
+                                >
+                                    Review{" "}
+                                    {selectedPlayList.reviews.text !== undefined
+                                        ? selectedPlayList.reviews.text
+                                        : "정보없음"}
+                                </div>
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
+        </div>
     );
 }
 
