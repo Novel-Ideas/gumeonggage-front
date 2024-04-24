aee7282f7d554a59b841bcdd2442f1e343ca54c8
dongyoon7212 <dongyoon7212@naver.com>
2024-04-25 00:09:53 +0900

    Feat: env 처리

--- src/apis/api/portOneApi.js
+++ src/apis/api/portOneApi.js
@@ -2,8 +2,8 @@ import * as PortOne from "@portone/browser-sdk/v2";
 
 export const portOnePayRequest = async ({ orderName, totalAmount }) => {
     return await PortOne.requestPayment({
-        storeId: "store-4333102a-9745-4a56-919e-c1f748208c81",
-        channelKey: "channel-key-3e68c9b8-63e2-4563-bbcf-591a2223d953",
+        storeId: process.env.REACT_APP_STORE_ID,
+        channelKey: process.env.REACT_APP_CHANNEL_KEY,
         paymentId: `mid_${new Date().getTime()}`,
         currency: "CURRENCY_KRW",
         orderName: orderName,
