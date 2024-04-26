f0cd93399a4106533732b51f16ee56112d3322d9
seohyeonwook <skut01541@gmail.com>
2024-04-26 15:05:26 +0900

    프론트엔드 회원탈퇴 구현

--- src/pages/adminPages/adminUserSearch/AdminUserSearch.jsx
+++ src/pages/adminPages/adminUserSearch/AdminUserSearch.jsx
@@ -1,25 +1,45 @@
 /**@jsxImportSource @emotion/react */
 import * as s from "./style";
 import { useState } from "react";
-import { useQuery } from "react-query";
-import { searchUserRequest } from "../../apis/api/searchUser";
-import AdminPageLayout from "../pageComponents/adminPageLayout/AdminPageLayout";
+import { useMutation, useQuery } from "react-query";
+import { searchUserRequest } from "../../../apis/api/searchUser";
 import { FaCircle } from "react-icons/fa";
+import AdminPageLayout from "../../../components/pageComponents/adminPageLayout/AdminPageLayout";
+import { deleteUserRequest } from "../../../apis/api/authApi";
 
 function AdminUserSearch() {
     const [userList, setUserList] = useState([]);
+    const [deleted, setDeleted] = useState(false);
 
     const searchUserQuery = useQuery(["searchUserQuery"], searchUserRequest, {
         retry: 0,
         refetchOnWindowFocus: false,
         onSuccess: (response) => {
             console.log(response.data);
-            setUserList(response.data);
+            setUserList(response.data); 
         },
         onError: (error) => {
             console.log(error);
         },
     });
+
+    const deleteUserMutation = useMutation({
+        mutationKey:"deleteUserMutation",
+        mutationFn: deleteUserRequest,
+        onSuccess: response => {
+            alert("삭제완료.");
+            window.location.replace("/admin/usersearch");
+        },
+        onError: error => {
+            console.error("삭제 요청 중 오류 발생:", error);
+        }
+    });
+
+    const handleDeleteUser = (userId) => {
+        console.log(userId);
+        deleteUserMutation.mutate(userId);
+        
+    };
 
     return (
         <AdminPageLayout>
@@ -42,6 +62,7 @@ function AdminUserSearch() {
                                     <li>전화번호</li>
                                     <li>포인트</li>
                                     <li>가입날짜</li>
+                                    <li>회원삭제</li>
                                 </ul>
                                 <div css={s.listLayout}>
                                     {userList.map((user, index) => (
@@ -51,6 +72,7 @@ function AdminUserSearch() {
                                             <li>{user.phonenumber}</li>
                                             <li>{user.totalPoint}</li>
                                             <li>{user.createDate}</li>
+                                            <li><button onClick={() => handleDeleteUser(user.userId)}>x</button></li>
                                         </ul>
                                     ))}
                                 </div>
