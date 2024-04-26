73a4cacb8013f5db39d60bd7c41cfbef67898326
dongyoon7212 <dongyoon7212@naver.com>
2024-04-26 16:12:49 +0900

    Feat: 토스페이 연동 실패

--- src/pages/payPages/paymentMethodPage/PaymentMethod.jsx
+++ src/pages/payPages/paymentMethodPage/PaymentMethod.jsx
@@ -13,9 +13,6 @@ function PaymentMethod() {
     const handleCancelClick = () => {
         navigate("/menu/menuall");
     };
-    // const handleDutchPayClick = () => {
-    //     navigate("/menu/menuall/order/dutchpay");
-    // };
 
     const handleOrderClick = () => {
         navigate("/menu/menuall/order/askpoint");
@@ -36,8 +33,12 @@ function PaymentMethod() {
                         </button>
                     </div>
                     <div css={s.methodLayout}>
-                        <button css={s.methodBox2} onClick={handleOrderClick} >
-                            <img src="https://blog.kakaocdn.net/dn/cEaPmw/btrcIUODymI/EBvA7nx7wVTcdLIrgiVsJK/img.jpg" alt="카카오페이" css={s.kakaoButton}/>
+                        <button css={s.methodBox2} onClick={handleOrderClick}>
+                            <img
+                                src="https://blog.kakaocdn.net/dn/cEaPmw/btrcIUODymI/EBvA7nx7wVTcdLIrgiVsJK/img.jpg"
+                                alt="카카오페이"
+                                css={s.kakaoButton}
+                            />
                         </button>
                     </div>
                 </div>
