14bee2f913a493330b97857d5b786846411e7464
dongyoon7212 <dongyoon7212@naver.com>
2024-04-26 17:49:53 +0900

    Feat: 스타일 수정 및 로직 수정

--- src/pages/adminPages/adminUserSearch/AdminUserSearch.jsx
+++ src/pages/adminPages/adminUserSearch/AdminUserSearch.jsx
@@ -3,23 +3,32 @@ import * as s from "./style";
 import { useState } from "react";
 import { useMutation, useQuery } from "react-query";
 import { searchUserRequest } from "../../../apis/api/searchUser";
-import { FaCircle } from "react-icons/fa";
+import { FaCircle, FaWindowClose } from "react-icons/fa";
 import AdminPageLayout from "../../../components/pageComponents/adminPageLayout/AdminPageLayout";
 import { deleteUserRequest } from "../../../apis/api/authApi";
-import { useQuery } from "react-query";
-import { FaCircle } from "react-icons/fa";
-import { searchUserRequest } from "../../../apis/api/searchUser";
+import Swal from "sweetalert2";
 
 function AdminUserSearch() {
     const [userList, setUserList] = useState([]);
     const [deleted, setDeleted] = useState(false);
 
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+        },
+    });
+
     const searchUserQuery = useQuery(["searchUserQuery"], searchUserRequest, {
         retry: 0,
         refetchOnWindowFocus: false,
         onSuccess: (response) => {
-            console.log(response.data);
-            setUserList(response.data); 
+            setUserList(response.data);
         },
         onError: (error) => {
             console.log(error);
@@ -27,21 +36,39 @@ function AdminUserSearch() {
     });
 
     const deleteUserMutation = useMutation({
-        mutationKey:"deleteUserMutation",
+        mutationKey: "deleteUserMutation",
         mutationFn: deleteUserRequest,
-        onSuccess: response => {
-            alert("삭제완료.");
-            window.location.replace("/admin/usersearch");
+        onSuccess: (response) => {
+            Toast.fire({
+                icon: "success",
+                title: "회원이 삭제되었습니다.",
+            });
+            setTimeout(() => {
+                window.location.replace("/admin/usersearch");
+            }, 2000);
         },
-        onError: error => {
+        onError: (error) => {
             console.error("삭제 요청 중 오류 발생:", error);
-        }
+        },
     });
 
     const handleDeleteUser = (userId) => {
-        console.log(userId);
-        deleteUserMutation.mutate(userId);
-        
+        Swal.fire({
+            title: "회원을 삭제하시겠습니까?",
+            text: "다시 되돌릴 수 없습니다. 신중하세요.",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "삭제",
+            cancelButtonText: "취소",
+            reverseButtons: true, // 버튼 순서 거꾸로
+        }).then((result) => {
+            if (result.isConfirmed) {
+                deleteUserMutation.mutate(userId);
+            }
+            return;
+        });
     };
 
     return (
@@ -70,12 +97,22 @@ function AdminUserSearch() {
                                 <div css={s.listLayout}>
                                     {userList.map((user, index) => (
                                         <ul key={index} css={s.askList}>
-                                            <li>{user.userId}</li>
+                                            <li>{index + 1}</li>
                                             <li>{user.roleNameKor}</li>
                                             <li>{user.phonenumber}</li>
                                             <li>{user.totalPoint}</li>
                                             <li>{user.createDate}</li>
-                                            <li><button onClick={() => handleDeleteUser(user.userId)}>x</button></li>
+                                            <li>
+                                                <button
+                                                    onClick={() =>
+                                                        handleDeleteUser(
+                                                            user.userId
+                                                        )
+                                                    }
+                                                >
+                                                    <FaWindowClose />
+                                                </button>
+                                            </li>
                                         </ul>
                                     ))}
                                 </div>
