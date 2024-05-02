8092ea45e197164317b80b6861068d2c70ef1df9
jayhyeon <jhkk0124@icloud.com>
2024-05-02 15:53:04 +0900

    281-상호명-변경-메뉴-디자인-수정

--- src/pages/adminPages/adminTradeNamePage/AdminTradeNamePage.jsx
+++ src/pages/adminPages/adminTradeNamePage/AdminTradeNamePage.jsx
@@ -8,13 +8,13 @@ import { editTradeNameRequest } from "../../../apis/api/tradeName";
 
 function AdminTradeNamePage() {
 
-    const [ newTradeName, setNewTradeName ] = useState("");
+    const [ tradeName, setTradeName ] = useState("");
 
     const editTradeNameMutation = useMutation({
         mutationKey: "editTradeNameMutation",
         mutationFn: editTradeNameRequest,
         onSuccess: response => {
-            setNewTradeName(() => response.data);
+            setTradeName(() => response.data);
             window.location.reload("/");
         },
         onError: error => {
@@ -25,7 +25,7 @@ function AdminTradeNamePage() {
 
     const handleOnClick = () => {
         editTradeNameMutation.mutate({
-            tradeName: newTradeName
+            tradeName: tradeName
         })
     }
     
@@ -39,6 +39,7 @@ function AdminTradeNamePage() {
                     <div css={s.inputLayout}>
                         <div>
                             <h1>변경 전 매장명</h1>
+                            <h1>: 구멍가게</h1>
                         </div>
                     </div>
                     <div css={s.arrowBox}>
